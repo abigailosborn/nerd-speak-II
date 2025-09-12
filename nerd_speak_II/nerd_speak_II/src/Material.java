b1ef74cb91da30e3c93d29b3c988581d008154a1
Abigail Osborn <114174326+abigailosborn@users.noreply.github.com>
2025-09-12 09:47:13 -0400

    Delete nerd_speak_II/nerd_speak_II directory

--- nerd_speak_II/nerd_speak_II/src/Material.java
+++
@@ -1,8 +0,0 @@
-public class Material{
-    public String name;
-    public String value;
-    public Material(String name, String value){
-        this.name = name;
-        this.value = value;
-    }
-}
--- nerd_speak_II/nerd_speak_II/src/Verbal.java
+++
@@ -1,8 +0,0 @@
-public class Verbal{
-    public String value;
-    public String name;
-    public Verbal(String name, String value){
-        this.name = name;
-        this.value = value;
-    }
-}
